9b479c399cc229d23e37b54011ed3acf53a8a18f
TolibjonIrisqulov <tolibjonirisqulov@gmail.com>
2024-07-20 00:01:40 +0500

    ha
    Irisqulov Tolibjon

---
+++ main.js
@@ -0,0 +1,121 @@
+function input7(){
+    var asos=document.getElementById('asos');
+     console.log(asos);
+     asos.value +=(7);
+ }
+ 
+ function input8(){
+     var asos=document.getElementById('asos');
+     console.log(asos);
+     asos.value +=(8);
+ }
+
+ function input9(){
+    var asos = document.getElementById('asos');
+     console.log(asos);
+     asos.value +=(9);
+ }
+
+ function input10(){
+    var asos = document.getElementById('asos');
+    console.log(asos);
+    asos.value +="+";
+ }
+
+ function salom1(){
+    var asos=document.getElementById('asos');
+     console.log(asos);
+     asos.value+=(4);
+ }
+
+ function salom2(){
+    var asos=document.getElementById('asos');
+     console.log(asos);
+     asos.value+=(5);
+ }
+
+ function salom3(){
+    var asos=document.getElementById('asos');
+     console.log(asos);
+     asos.value+=(6);
+ }
+
+ function book(){
+    var asos=document.getElementById('asos');
+     console.log(asos);
+     asos.value+="*";
+ }
+
+ function div1(){
+    var asos=document.getElementById('asos');
+     console.log(asos);
+     asos.value+=(1);
+ }
+
+ function div2(){
+    var asos=document.getElementById('asos');
+     console.log(asos);
+     asos.value+=(2);
+ }
+
+ function div3(){
+    var asos=document.getElementById('asos');
+     console.log(asos);
+     asos.value+=(3);
+ }
+
+ function inputa(){
+    var asos=document.getElementById('asos');
+     console.log(asos);
+     asos.value+="-";
+ }
+
+ function div4(){
+    var asos=document.getElementById('asos');
+     console.log(asos);
+     asos.value+=(0);
+ }
+
+ function books(){
+    var asos=document.getElementById('asos');
+     console.log(asos);
+     asos.value+=".";
+ }
+
+ function booked(){
+    var asos=document.getElementById('asos');
+     console.log(asos);
+     asos.value+="/";
+ }
+
+ function value1(){
+    var asos=document.getElementById('asos');
+     console.log(asos.value);
+     var natija = eval(asos.value);
+     console.log(natija);
+     asos.value = natija;
+ }
+
+ function inputac(){
+    var asos=document.getElementById('asos');
+     console.log(asos);
+     asos.value -="AC";
+ }
+
+ function ustoz(){
+    var asos=document.getElementById('asos');
+     console.log(asos);
+     asos.value +="+/-";
+ }
+
+ function ustoz1(){
+    var asos=document.getElementById('asos');
+     console.log(asos);
+     asos.value +="%";
+ }
+
+ function ustoz2(){
+    var asos=document.getElementById('asos');
+     console.log(asos);
+     asos.value =" ";
+ }
